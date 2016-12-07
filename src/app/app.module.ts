053f0efb4bfc96695a8b8c540beee0a861f68384
sfcst2 <sfcst2@gmail.com>
2016-12-07 15:35:03 -0500

    Adding in D3 vertical bar chart example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { NG2D3Module } from 'ng2d3';
 
 import { AppComponent } from './app.component';
+import { ForcedGraphComponent } from './forced-graph/forced-graph.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ForcedGraphComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule, 
+    NG2D3Module
   ],
   providers: [],
   bootstrap: [AppComponent]
